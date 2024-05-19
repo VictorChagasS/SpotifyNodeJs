9183eafa507219f60037fb469e1196f6b0dc3ae7
VictorChagasS <victorsantoschagas@hotmail.com>
2024-05-19 10:22:09 -0300

    Branch 2

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 
 //Add comment test 2
+//ESTOU NA BRANCH 2
 
 // CONFIGURACAO DO HANDLEBARS
 handlebarsConfig(app);
