36bd06d66c264294aaae819ecaadf97533b92282
VictorChagasS <victorsantoschagas@hotmail.com>
2024-05-19 10:18:36 -0300

    Add comment 2

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 
-//Add comment test
+//Add comment test 2
 
 // CONFIGURACAO DO HANDLEBARS
 handlebarsConfig(app);
